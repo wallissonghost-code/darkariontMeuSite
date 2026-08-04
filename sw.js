62c733aa2a06e4df92254387461c874e0afbcb33
wallissonghost-code <wallissonghost@gmail.com>
2026-08-04 00:03:35 -0300

    chore(cache): ativa cache 3.4.8

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const RELEASE='wd-founder-3.4.7';
+const RELEASE='wd-founder-3.4.8';
 const STATIC_CACHE=`${RELEASE}-static`;
 const RUNTIME_CACHE=`${RELEASE}-runtime`;
 const OFFLINE_FALLBACK='index.html';
