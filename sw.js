3011a9d79b70233b71377f336364ecc34455b39f
wallissonghost-code <wallissonghost@gmail.com>
2026-08-04 00:53:12 -0300

    chore: renovar cache para Rank 3.7.0

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const RELEASE='wd-founder-3.6.1';
+const RELEASE='wd-founder-3.7.0';
 const STATIC_CACHE=`${RELEASE}-static`;
 const RUNTIME_CACHE=`${RELEASE}-runtime`;
 const OFFLINE_FALLBACK='index.html';
