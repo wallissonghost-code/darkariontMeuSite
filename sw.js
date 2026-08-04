55fe99ee79336658b0ccfb20c91b7e9f8e19e0a4
wallissonghost-code <wallissonghost@gmail.com>
2026-08-03 23:34:39 -0300

    chore: renovar cache da versão 3.4.4

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const RELEASE='wd-founder-3.4.3';
+const RELEASE='wd-founder-3.4.4';
 const STATIC_CACHE=`${RELEASE}-static`;
 const RUNTIME_CACHE=`${RELEASE}-runtime`;
 const OFFLINE_FALLBACK='index.html';
